08ed9de76cdde311ff1800184439e0ffeba38d23
Aman katiyar <amankatiyar783597@gmail.com>
2025-02-14 01:09:47 +0530

    95%setup

--- src/Server.js
+++ src/Server.js
@@ -1,7 +1,18 @@
 import { configDotenv } from 'dotenv';
 import ConnetDB from './db/db.js';
+import { app } from './App.js';
 
 configDotenv({ path: './.env' });
 
-ConnetDB()
+const port = process.env.PORT || 3000;
 
+ConnetDB()
+.then( ()=> 
+    app.listen(port, () => {
+        console.log("Server is running at : " , port)
+    })
+)
+.catch( (err) => 
+    console.log("MonGo DB connection falied ", err)
+)
+ 
